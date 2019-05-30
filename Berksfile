a9ffd5b213f2945b68e15b6147bbfca7802a14ad
Tech-A34 <tech-a34@Tech-A34s-MacBook-Air.local>
2019-05-30 16:41:32 +0100

    initialised

--- Berksfile
+++ Berksfile
@@ -1,4 +1,8 @@
 # frozen_string_literal: true
 source 'https://supermarket.chef.io'
 
+cookbook 'python', git: 'https://github.com/aehussey/Chef-Python-Cookbook'
+
+cookbook 'nginx_reverse_proxy', git: 'https://github.com/aehussey/Chef-Nginx-Reverse-proxy'
+
 metadata
--- spec/unit/recipes/default_spec.rb
+++ spec/unit/recipes/default_spec.rb
@@ -11,20 +11,7 @@ describe 'UberAppProject::default' do
     let(:chef_run) do
       # for a complete list of available platforms and versions see:
       # https://github.com/customink/fauxhai/blob/master/PLATFORMS.md
-      runner = ChefSpec::ServerRunner.new(platform: 'ubuntu', version: '16.04')
-      runner.converge(described_recipe)
-    end
-
-    it 'converges successfully' do
-      expect { chef_run }.to_not raise_error
-    end
-  end
-
-  context 'When all attributes are default, on CentOS 7.4.1708' do
-    let(:chef_run) do
-      # for a complete list of available platforms and versions see:
-      # https://github.com/customink/fauxhai/blob/master/PLATFORMS.md
-      runner = ChefSpec::ServerRunner.new(platform: 'centos', version: '7.4.1708')
+      runner = ChefSpec::SoloRunner.new(platform: 'ubuntu', version: '16.04')
       runner.converge(described_recipe)
     end
 
