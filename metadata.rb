12d13ce1f7caa95770e95e109f33d92f33ea7a84
Tech-A34 <tech-a34@Tech-A34s-MacBook-Air.local>
2019-05-31 11:19:47 +0100

    bug fix for node cookbook

--- metadata.rb
+++ metadata.rb
@@ -18,3 +18,5 @@ chef_version '>= 13.0'
 # a Supermarket.
 #
 # source_url 'https://github.com/<insert_org_here>/UberAppProject'
+
+depends 'node'
--- recipes/default.rb
+++ recipes/default.rb
@@ -3,3 +3,4 @@
 # Recipe:: default
 #
 # Copyright:: 2019, The Authors, All Rights Reserved.
+include_recipe "node::default"
