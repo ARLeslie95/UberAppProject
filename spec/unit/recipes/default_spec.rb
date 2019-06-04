184f19dcc093f05fe03bf1e1e53fd2841b1c02ae
Tech-A34 <tech-a34@Tech-A34s-MacBook-Air.local>
2019-06-04 09:15:52 +0100

    test

--- spec/unit/recipes/default_spec.rb
+++ spec/unit/recipes/default_spec.rb
@@ -7,11 +7,11 @@
 require 'spec_helper'
 
 describe 'UberAppProject::default' do
-  context 'When all attributes are default, on Ubuntu 16.04' do
+  context 'When all attributes are default, on Ubuntu 18.04' do
     let(:chef_run) do
       # for a complete list of available platforms and versions see:
       # https://github.com/customink/fauxhai/blob/master/PLATFORMS.md
-      runner = ChefSpec::SoloRunner.new(platform: 'ubuntu', version: '16.04')
+      runner = ChefSpec::SoloRunner.new(platform: 'ubuntu', version: '18.04')
       runner.converge(described_recipe)
     end
 
@@ -55,5 +55,6 @@ describe 'UberAppProject::default' do
     it 'should upgrade python-pip' do
       expect(chef_run).to upgrade_package('python-pip')
     end
+    at_exit { ChefSpec::Coverage.report! }
   end
 end
