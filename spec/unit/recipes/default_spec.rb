d597b295a0c2741a0f07567f23b6c368ec05e50e
Tech-A34 <tech-a34@Tech-A34s-MacBook-Air.local>
2019-06-04 14:45:50 +0100

    test

--- spec/unit/recipes/default_spec.rb
+++ spec/unit/recipes/default_spec.rb
@@ -9,11 +9,11 @@ require 'spec_helper'
 
 describe 'UberAppProject::default' do
 
-  context 'When all attributes are default, on Ubuntu 16.04' do
+  context 'When all attributes are default, on Ubuntu 18.04' do
     let(:chef_run) do
       # for a complete list of available platforms and versions see:
       # https://github.com/customink/fauxhai/blob/master/PLATFORMS.md
-      runner = ChefSpec::SoloRunner.new(platform: 'ubuntu', version: '16.04')
+      runner = ChefSpec::SoloRunner.new(platform: 'ubuntu', version: '18.04')
       runner.converge(described_recipe)
     end
 
